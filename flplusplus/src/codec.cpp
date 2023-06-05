dc7e649a2bf052017f3905f7f6cb31b9c0bf6243
C.S. Melis <cees3331x00@gmail.com>
2023-06-05 15:05:23 +0200

    Get base addresses for soundmanager and soundstreamer
    Using the hardcoded virtual addresses will work most of the time. However, I've seen cases where one of the DLLs get moved (loaded at a different base address). As a result, the patch will be applied at the incorrect address, and in some cases the game may even crash. Therefore, it's better to play it safe and always get the base address to guarantee the patch will be correct.

--- flplusplus/src/codec.cpp
+++ flplusplus/src/codec.cpp
@@ -2,9 +2,15 @@
 #include "offsets.h"
 #include "patch.h"
 
+#define WIN32_LEAN_AND_MEAN
+#include "windows.h"
+
 void codec::init()
 {
+    DWORD soundManager = (DWORD) GetModuleHandleA("soundmanager.dll");
+    DWORD soundStreamer = (DWORD) GetModuleHandleA("soundstreamer.dll");
+
     //Patch out MP3 warnings
-    patch::patch_uint8(OF_SOUNDMAN_MP3, 0xC3);
-    patch::patch_uint8(OF_SOUNDSTR_MP3, 0xC3);
+    patch::patch_uint8(soundManager + F_OF_SOUNDMAN_MP3, 0xC3);
+    patch::patch_uint8(soundStreamer + F_OF_SOUNDSTR_MP3, 0xC3);
 }
