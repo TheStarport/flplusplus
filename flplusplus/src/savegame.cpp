d1747198ff1f1857668b6cbc5fa7dfc6fa22c3c8
C.S. Melis <cees3331x00@gmail.com>
2024-04-16 01:16:30 +0200

    Update UserDataPath function
    Updated UserDataPath to make the function act more like the way it does in Common.dll. Possibly resolves the 'invalid account name' error from https://github.com/BC46/freelancer-hd-edition/issues/104.

--- flplusplus/src/savegame.cpp
+++ flplusplus/src/savegame.cpp
@@ -1,6 +1,7 @@
 #include "savegame.h"
 #include "config.h"
 #include "patch.h"
+#include "log.h"
 
 #define WIN32_LEAN_AND_MEAN
 #define _CRT_SECURE_NO_WARNINGS
@@ -8,8 +9,16 @@
 #include <cstring>
 #include <shlwapi.h>
 #include <shlobj.h>
+#include <io.h>
+#include <direct.h>
 
-bool UserDataPath(char *outputBuffer)
+void HandleUserDataPathFail(char * const outputBuffer)
+{
+    *outputBuffer = '\0';
+    logger::writeline("flplusplus: failed to access the saves directory. Freelancer may not be able to properly store save files.");
+}
+
+bool UserDataPath(char * const outputBuffer)
 {
     char path[MAX_PATH];
     if(config::get_config().saveindirectory) {
@@ -17,10 +26,30 @@ bool UserDataPath(char *outputBuffer)
         PathRemoveFileSpecA(path);
         PathAppendA(path, "..\\SAVE");
     } else {
-        SHGetFolderPathA(NULL, CSIDL_PERSONAL | CSIDL_FLAG_CREATE, NULL, 0, path);
+        if (SHGetFolderPathA(NULL, CSIDL_PERSONAL | CSIDL_FLAG_CREATE, NULL, 0, path) != S_OK) {
+            HandleUserDataPathFail(outputBuffer);
+            return false;
+        }
+
         PathAppendA(path, "My Games");
+
+        if (_access(path, 0) != 0) {
+            if (_mkdir(path) != 0) {
+                HandleUserDataPathFail(outputBuffer);
+                return false;
+            }
+        }
+
         PathAppendA(path, config::get_config().savefoldername.c_str());
     }
+
+    if (_access(path, 0) != 0) {
+        if (_mkdir(path) != 0) {
+            HandleUserDataPathFail(outputBuffer);
+            return false;
+        }
+    }
+
     strcpy(outputBuffer, path);
     return true;
 }
